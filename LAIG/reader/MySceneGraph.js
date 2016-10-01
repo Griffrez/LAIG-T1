98c6a0fb706307232af4ce3672594f120d5e8419
Eduardo Leite <xJupiter931@gmail.com>
2016-10-01 19:12:18 +0100

    Scene Parsing Done

--- LAIG/reader/MySceneGraph.js
+++ LAIG/reader/MySceneGraph.js
@@ -1,5 +1,6 @@
 
-function MySceneGraph(filename, scene) {
+function MySceneGraph(filename, scene)
+{
 	this.loadedOk = null;
 	
 	// Establish bidirectional references between scene and graph
@@ -26,55 +27,58 @@ MySceneGraph.prototype.onXMLReady=function()
 	console.log("XML Loading finished.");
 	var rootElement = this.reader.xmlDoc.documentElement;
 
+	/*
 	this.components = new Components();
 	this.materials	= [];
 	this.lights		= [];
+	*/
 	
 	// Here should go the calls for different functions to parse the various blocks
-	var error = this.parseScene(rootElement);
+	// var error = this.parseScene(rootElement);
+	var error = this.parseDSX(rootElement);
 
-	if (error != null) {
+	if (error != null)
+	{
 		this.onXMLError(error);
 		return;
 	}	
 
-	this.loadedOk=true;
+	this.loadedOk = true;
 	
 	// As the graph loaded ok, signal the scene so that any additional initialization depending on the graph can take place
 	this.scene.onGraphLoaded();
 };
 
-
+MySceneGraph.prototype.parseDSX = function(rootElement)
+{
+	// Parse Scene
+	if(parseScene(rootElement) == null)
+		return "Error Parsing Scene";
+};
 
 /*
  * Example of method that parses components of one block and stores information in a specific data structure
  */
-MySceneGraph.prototype.parseScene= function(rootElement) {
-	
+MySceneGraph.prototype.parseScene= function(rootElement)
+{
 	var elems = rootElement.getElementsByTagName('scene');
-	if (elems == null) {
-		return "scene element is missing.";
-	}
-
-	if (elems.length != 1) {
-		return "either zero or more than one 'scene' element found.";
-	}
-
-	var scene = elems[0];
-
-	var id = this.reader.getString(scene, "root");
-	this.components.addComponent(id);
-
-	this.axisLength = this.reader.getFloat(scene, "axis_length");
+	
+	// Testing goes here
+	
+	var root = this.reader.getString(elems[0], 'root');
+	var axisLength = this.reader.getFloat(elems[0], 'axis_length');
+	
+	this.myScene = new MyScene(root,axisLength);
 };
 	
 /*
  * Callback to be executed on any read error
  */
  
-MySceneGraph.prototype.onXMLError=function (message) {
-	console.error("XML Loading Error: "+message);	
-	this.loadedOk=false;
+MySceneGraph.prototype.onXMLError=function (message)
+{
+	console.error("XML Loading Error: " + message);	
+	this.loadedOk = false;
 };
 
 
---
+++ LAIG/reader/Scene.js
@@ -0,0 +1,28 @@
+// Constructor
+function Scene(root, axisLength)
+{
+	this.root = root;
+	this.axisLength;
+};
+
+// Gets
+Scene.prototype.getRoot = function()
+{
+	return this.root;
+};
+
+Scene.prototype.getRoot = function()
+{
+	return this.root;
+};
+
+// Sets
+Scene.prototype.getRoot = function(root)
+{
+	this.root = root;
+};
+
+Scene.prototype.getRoot = function(axisLenght)
+{
+	this.axisLength;
+};
